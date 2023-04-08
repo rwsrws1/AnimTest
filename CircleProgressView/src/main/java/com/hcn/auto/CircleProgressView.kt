35a659de56ee052ac025868765c7cac078eb387f
xut <xutao@hcn2000.com>
2023-04-08 12:11:09 +0800

    1.0

---
+++ CircleProgressView/src/main/java/com/hcn/auto/CircleProgressView.kt
@@ -0,0 +1,80 @@
+package com.hcn.auto
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.View
+import com.hcn.auto.circleprogressview.R
+
+class CircleProgressView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+    private var mCircleColor = Color.argb(0x80, 0x92, 0x94, 0x95)
+    private var mRIngColor = Color.argb(0xff, 0xff, 0xe0, 0x16)
+    private var mRadius = 103F
+    private var mRingRadius = 103F
+    private var mStrokeWidth = 3F
+    private var mProgress = 0F
+    private var mTotalProgress = 100F
+    private val mCirclePaint = Paint()
+    private val mRingPaint = Paint()
+
+    init {
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.CircleProgressView)
+        mCircleColor = typedArray.getColor(R.styleable.CircleProgressView_mCircleColor, mCircleColor)
+        mRIngColor = typedArray.getColor(R.styleable.CircleProgressView_mRingColor, mRIngColor)
+        mRadius = typedArray.getDimension(R.styleable.CircleProgressView_mRadius, mRadius)
+        mRingRadius = typedArray.getDimension(R.styleable.CircleProgressView_mRingRadius, mRingRadius)
+        mStrokeWidth = typedArray.getDimension(R.styleable.CircleProgressView_mStrokeWidth, mStrokeWidth)
+        mProgress = typedArray.getDimension(R.styleable.CircleProgressView_mProgress, mProgress)
+        mTotalProgress = typedArray.getDimension(R.styleable.CircleProgressView_mTotalProgress, mTotalProgress)
+
+        mCirclePaint.apply {
+            isAntiAlias = true
+            style = Paint.Style.STROKE
+            color = mCircleColor
+            strokeWidth = mStrokeWidth
+        }
+
+        mRingPaint.apply {
+            isAntiAlias = true
+            color = mRIngColor
+            style = Paint.Style.STROKE
+            strokeWidth = mStrokeWidth
+        }
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        val mXCenter = width/2F
+        val mYCenter = height/2F
+        canvas.drawCircle(mXCenter, mYCenter, mRadius, mCirclePaint)
+
+        if (mProgress > 0) {
+            val oval = RectF()
+            oval.apply {
+                left = mXCenter - mRingRadius
+                top = mYCenter - mRingRadius
+                right = mRingRadius * 2 + left
+                bottom = mRingRadius * 2 + top
+                canvas.drawArc(oval, -90F, (mProgress/mTotalProgress) * 360F, false, mRingPaint)
+            }
+        }
+    }
+
+    private fun sp2px(sp: Int): Float {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp.toFloat(), resources.displayMetrics)
+    }
+
+    private fun dip2px(dip: Int): Float {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dip.toFloat(), resources.displayMetrics)
+    }
+
+    fun setProgress(progress: Int) {
+        mProgress = progress.toFloat()
+        postInvalidate()
+    }
+}
